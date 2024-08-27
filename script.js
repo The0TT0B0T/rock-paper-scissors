f5592676be5c076cd99fa0d2954dae02a9f81d1b
Otto Ayala <oayala543@gmail.com>
2024-08-27 17:57:22 -0500

    Finished the game and added a winning message depending which player had the the bigger score.

--- script.js
+++ script.js
@@ -7,17 +7,18 @@ let computerScore = 0;
 const getComputerChoice = () => {
     const random = Math.floor(Math.random() * 3) + 1
     if (random === 1) {
-        console.log(random);
+        
         return 'Rock';
     }
     else if (random === 2) {
-        console.log(random);
+        
         return 'Paper';
     }
     else { 
         return 'Scissors';
     }
 }
+
 
 const getHumanChoice = () => {
     const input = prompt("Enter a rock, paper, or scissors").toLowerCase();
@@ -39,8 +40,7 @@ const getHumanChoice = () => {
 //humanChoice and computerChoice are parameters for the human computerChoice and humanChoice as arguments
 
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
+
 
 const playGame = () => {
     let humanScore = 0;
@@ -61,13 +61,23 @@ const playGame = () => {
              return 'Computer wins this round!';
          }
      };
-     for (let i = 0; i <= 5; i++) {
-        playRound();
+     
+
+     for (let i = 1; i <=5; i++) {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        console.log(playRound(humanChoice, computerChoice));
+        console.log(`Your Score = ${humanScore}`);
+        console.log(`Computer's Score = ${computerScore}`);
+        
+     }
+     if(humanScore > computerScore) {
+        console.log("You win the Game!");
+     }
+     else {
+        console.log("Game Over! Computer Wins!");
      }
 };
+playGame();
 
-console.log(playGame());
-console.log(playRound(humanChoice, computerChoice));
-console.log(humanScore);
-console.log(computerScore);
 
