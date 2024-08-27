7a53b3d0b9f16c2c29472f12b32d62c1c831bcbe
Otto Ayala <oayala543@gmail.com>
2024-08-26 19:12:09 -0500

    functionality for rounds done and now just have to finish the play game function to end at 5 rounds

--- script.js
+++ script.js
@@ -7,28 +7,67 @@ let computerScore = 0;
 const getComputerChoice = () => {
     const random = Math.floor(Math.random() * 3) + 1
     if (random === 1) {
+        console.log(random);
         return 'Rock';
     }
     else if (random === 2) {
+        console.log(random);
         return 'Paper';
     }
-    else return 'Scissors';
+    else { 
+        return 'Scissors';
+    }
 }
 
 const getHumanChoice = () => {
-    const input = prompt("Enter a rock, paper, or scissors");
-    if(input === 'Rock') {
+    const input = prompt("Enter a rock, paper, or scissors").toLowerCase();
+    if(input === 'rock') {
         return 'Rock';
     }
-    else if (input === 'Paper') {
+    else if (input === 'paper') {
         return 'Paper';
     }
-    else return 'Scissors';
+    else if (input === 'scissors') {
+        return 'Scissors';
+    } 
+    else {
+        alert("Invalid choice. Please enter rock, paper, or scissors.");
+        return 'Invalid choice';
+    }
 };
 
-const playRound = (humanChoice, computerChoice) => {
-    
+//humanChoice and computerChoice are parameters for the human computerChoice and humanChoice as arguments
+
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+const playGame = () => {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    const playRound = (humanChoice, computerChoice) => {
+        if (humanChoice === computerChoice) {
+         return `It's a tie! Play Again`;
+        }
+        else if ((humanChoice === 'Rock' && computerChoice === 'Scissors') ||
+                 (humanChoice === 'Paper' && computerChoice === 'Rock') ||
+                 (humanChoice === 'Scissors' && computerChoice === 'Paper')) {
+                     humanScore++;
+                     return 'You win this round!';
+                 }
+         else {
+             computerScore++;
+             return 'Computer wins this round!';
+         }
+     };
+     for (let i = 0; i <= 5; i++) {
+        playRound();
+     }
 };
 
-console.log(getHumanChoice());
-console.log(getComputerChoice());
+console.log(playGame());
+console.log(playRound(humanChoice, computerChoice));
+console.log(humanScore);
+console.log(computerScore);
+
